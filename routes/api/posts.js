9c6eae5df2c103e85cad95421b2fe9893cf365b4
Murrayw <williams.murray@gmail.com>
2019-02-06 21:30:20 +0200

    Created route to create new post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Post = require('../../models/Post')
 
 // @route 	GET /api/users/posts
 // @desc 		Tests post route
 // @access 	Public
-router.get("/", (req, res) => res.json({ msg: "Posts Works" }));
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
-// @route 	GET /api/posts/
-// @desc 		Tests post route
-// @access 	Public
+// @route 	POST /api/posts/
+// @desc 		Create a post
+// @access 	Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) {
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  })
+
+  newPost.save().then(post => res.json(post))
+})
 
 module.exports = router;

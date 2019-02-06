5440b4e4d26d3c63be228e58ef6168333f2e75c4
Murrayw <williams.murray@gmail.com>
2019-02-06 21:11:13 +0200

    Added Post model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // @route 	GET /api/users/posts
 // @desc 		Tests post route
 // @access 	Public
-router.get('/', (req, res) => res.json({ msg: 'Posts Works' }));
+router.get("/", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route 	GET /api/posts/
+// @desc 		Tests post route
+// @access 	Public
 
 module.exports = router;

d88fb98d2dc61dab52d26fddc4f74ad0f34f0181
Murrayw <williams.murray@gmail.com>
2019-02-09 19:57:07 +0200

    Setup login reducer + set token to localstorage

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,6 @@
+import { SET_CURRENT_USER } from '../actions/types';
+import isEmpty from '../validation/is-empty';
+
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +8,14 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        // Check that payload is not empty
+        isAuthenticated: !isEmpty(action.payload),
+        // If the above passes then set user to payload
+        user: action.payload
+      };
     default:
       return state;
   }

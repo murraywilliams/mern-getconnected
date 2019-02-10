8901c412ecd14f1897892a062db75f3fba8e1016
Murrayw <williams.murray@gmail.com>
2019-02-10 19:16:05 +0200

    Added common component Input group for social icons

---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange
+}) => {
+  return (
+    <div className='form-group'>
+      <input
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {info && <small className='form-text text-muted'>{info}</small>}
+      {error && <div className='invalid-feedback'>{error}</div>}
+    </div>
+  );
+};
+
+InputGroup.defaultProps = {
+  type: 'text'
+};
+
+InputGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  icon: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default InputGroup;

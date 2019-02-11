50f2df46a48400d0ae3d57906e4f8e3528356a25
Murrayw <williams.murray@gmail.com>
2019-02-11 22:57:51 +0200

    Added post, comment actions + integrated into UI

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,8 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 
 import './App.css';
 import { decode } from 'punycode';
@@ -91,6 +93,12 @@ class App extends Component {
                   path='/add-education'
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path='/feed' component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path='/post/:id' component={Post} />
               </Switch>
               <Route exact path='/not-found' component={NotFound} />
             </div>
---
+++ client/src/components/post/Post.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import PostItem from '../posts/PostItem';
+import Spinner from '../common/Spinner';
+import { getPost } from '../../actions/postActions';
+import isEmpty from '../../validation/is-empty';
+
+class Post extends Component {
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+  }
+  render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post === null || loading || isEmpty(post)) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
+
+    return (
+      <div className='post'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <Link to='/feed' className='btn btn-light mb-2'>
+                Back To Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);

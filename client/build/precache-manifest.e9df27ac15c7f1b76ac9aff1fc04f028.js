54b135b127bf9e3d98031b4b038a7e8f3a9bf7c8
Murrayw <williams.murray@gmail.com>
2019-02-12 09:17:31 +0200

    Added /build to commit

---
+++ client/build/precache-manifest.e9df27ac15c7f1b76ac9aff1fc04f028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad47d3ccd678c11e177c",
+    "url": "/static/css/main.5cb3420d.chunk.css"
+  },
+  {
+    "revision": "ad47d3ccd678c11e177c",
+    "url": "/static/js/main.ad47d3cc.chunk.js"
+  },
+  {
+    "revision": "d6ef7cb7f7fce42bcfc5",
+    "url": "/static/js/1.d6ef7cb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "2d5ea608a987c8403e78ff3d8d9b5200",
+    "url": "/index.html"
+  }
+];
